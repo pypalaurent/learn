8372bf01d4832f8e167d37796778e970526154d0
Pypa Laurent <pypalaurent@gmail.com>
2022-10-31 18:26:43 -0400

    ading docstrings and document each function

--- apprentice/pyfund/words.py
+++ apprentice/pyfund/words.py
@@ -1,9 +1,25 @@
 from urllib.request import urlopen
 import sys
 
+"""Retrive and print words from a URL
+
+Usage:
+
+python words.py <URL>
+
+"""
 
 # this fetches the words and returns them as a list
 def fetch_words(url):
+    """Fetch a list of words from a URL
+
+    Args:
+        url: The URL of a UTF-8 text document
+
+    Returns:
+        A list of strings containing the words from
+        the document
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -15,11 +31,21 @@ def fetch_words(url):
 
 # this prints a list of words
 def print_items(items):
+    """Print one item per lin
+
+    Args:
+        items: An iterable series of printable items
+    """
     for item in items:
         print(item)
 
 
 def main(url):
+    """Retrieve and ptint words from URL
+
+    Usage:
+        url: The URL of a UTF-8 text document
+    """
     words = fetch_words(url)
     print_items(words)
 
